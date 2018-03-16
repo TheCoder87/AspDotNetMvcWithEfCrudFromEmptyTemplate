dd51a376bab5b1a7caea38ba0e45379501037292
TheCoder87 <thecoder87@hotmail.com>
2018-03-16 17:17:34 +0530

    Created LoginModel and Login form

---
+++ AspDotNetMvcWithEfCrudFromEmptyTemplate/Models/LoginModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspDotNetMvcWithEfCrudFromEmptyTemplate.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string EmailId { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [HiddenInput(DisplayValue = false)]
+        public string ReturnUrl { get; set; }
+    }
+}
