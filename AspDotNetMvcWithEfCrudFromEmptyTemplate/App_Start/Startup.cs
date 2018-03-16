588867972c00447d916ea9fefcf01894182db764
TheCoder87 <thecoder87@hotmail.com>
2018-03-16 16:32:24 +0530

    Coded Authentication Middleware in Startup.cs

---
+++ AspDotNetMvcWithEfCrudFromEmptyTemplate/App_Start/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+
+namespace AspDotNetMvcWithEfCrudFromEmptyTemplate.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "ApplicationCookie",
+                LoginPath = new PathString("/auth/login")
+            });
+        }
+    }
+}
